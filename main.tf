18d76e82374c439a1dc7a6d17d1acf7824468fab
atehrene <atehrene1993@gmail.com>
2023-03-24 20:38:32 -0400

    fifth commit

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
   region = "us-west-1"
 }
+
 resource "aws_vpc" "actions" { 
-    cidr_block = "10.0.0.0/16"
+   cidr_block = "10.0.0.0/16"
 
     tags = {
       Name = "class30"
- }
+    }
 }
 
